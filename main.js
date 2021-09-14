ac4f223b015b782e4ed54e4ac199077e5ef2a1a8
Tchilo <tchiloross@gmail.com>
2021-09-14 10:54:15 +0200

    Add books to local storage

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 let books = [];
 const list = document.querySelector('.list');
 
@@ -7,27 +5,24 @@ const addBook = () => {
   const book = {
     title: document.querySelector('.title').value,
     author: document.querySelector('.author').value,
-    id: books.length,
+    // id: books.length,
   };
   books.push(book);
+  const booksInformation = JSON.stringify(books);
+  localStorage.setItem('Collection', booksInformation);
 };
 
-
-
-
-
-
 const display = () => {
-  // const title = document.createElement('h4');
-  // const author = document.createElement('li');
-
   const bookInfo = document.createElement('li');
   const remove = document.createElement('button');
+  const bookData = localStorage.getItem('collection');
 
+  const data = JSON.parse(bookData);
+  if (data) {
+    books = data;
+  }
   books.forEach((book) => {
-    
-    
-    remove.innerHTML = `delete`;
+    remove.innerHTML = 'delete';
 
     bookInfo.innerHTML = `&nbsp; ${book.author} &nbsp;&nbsp; ${book.title} &nbsp;&nbsp; `;
 
@@ -42,46 +37,34 @@ const display = () => {
   });
 };
 
-
-
 const addButton = document.querySelector('.sub');
 addButton.addEventListener('click', () => {
   addBook();
   display();
 });
 
-const deleteBook =  (index) => {
-
-  books = books.filter(book => book !== books[index]);
+const deleteBook = (index) => {
+  books = books.filter((book) => book !== books[index]);
 
   return books;
-
-}
+};
 
 // const removes = document.querySelectorAll('remove');
 
+// [...list.children].forEach(child, index => {
 
-[...list.children].forEach(child, index => {
+//     child.addEventListener('click', (e) => {
 
-  child.addEventListener('click',   (e) => {
+//         if (e.target.classList.contains('remove')) {
 
-    if(e.target.classList.contains('remove')){
+//             deleteBook(index);
+//             e.target.parentElement.remove();
+//         }
 
-      deleteBook(index);
-      e.target.parentElement.remove();
-    }
+//         // deleteBook(index);
+//         // remove.parentElement.parentElement.remove();
+//         console.log(books);
 
+//     });
 
-      // deleteBook(index);
-      // remove.parentElement.parentElement.remove();
-      console.log(books);
-
-
-  });
-
-});
-
-
-
-
-
+// });
